96b29017a77fd9a01fdfd694bcdf62975433a75e
Daniel Miranda <danielmmiranda@gmail.com>
2023-07-24 18:04:32 -0300

    aula 15 update

--- exercicios/aula15/ambiente.js
+++ exercicios/aula15/ambiente.js
@@ -1,5 +1,11 @@
 let num = [5, 8, 3 , 2 , 9]
+num.sort()
 num.push(1)
-let ord = num.length
-console.log(`Nosso vetor ${num} tem ${ord} posições`)
+console.log(`Nosso vetor ${num} tem ${num.length} posições`)
 console.log(`O primeiro valor é ${num[0]}`)
+let pos = num.indexOf(2)
+if (pos == -1) {
+    console.log("O valor não foi encontrado")
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
--- exercicios/aula15/vetor.js
+++ exercicios/aula15/vetor.js
@@ -1,5 +1,12 @@
-let valores = [0,1,7,2,9]
+let num = [0, 1, 7, 2, 9]
 console.log(num)
-for (let pos = 0; pos < valores.length; pos++) {
-    console.log(num[pos])
+
+//for...
+/*for (let pos = 0; pos < num.length; pos++) {
+    console.log(` A posição ${pos} tem o valor ${num[pos]}`)
+}*/
+
+//for... in
+for (let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }
