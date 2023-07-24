e4a735b8675cf78589ec92b2eb918e7d2e9835d7
Daniel Miranda <danielmmiranda@gmail.com>
2023-07-24 17:31:51 -0300

    vetores

--- exercicios/aula15/ambiente.js
+++ exercicios/aula15/ambiente.js
@@ -1,3 +1,5 @@
-let num
-
-consol.log(`Nosso vetor é o $(num)`)
+let num = [5, 8, 3 , 2 , 9]
+num.push(1)
+let ord = num.length
+console.log(`Nosso vetor ${num} tem ${ord} posições`)
+console.log(`O primeiro valor é ${num[0]}`)
---
+++ exercicios/aula15/vetor.js
@@ -0,0 +1,5 @@
+let valores = [0,1,7,2,9]
+console.log(num)
+for (let pos = 0; pos < valores.length; pos++) {
+    console.log(num[pos])
+}
