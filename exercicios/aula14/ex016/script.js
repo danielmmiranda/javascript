8b23f3e46628a1af4f0d1c323f622d6f9b2e1220
Daniel Miranda <danielmmiranda@gmail.com>
2023-07-24 01:48:47 -0300

    Update script.js

--- exercicios/aula14/ex016/script.js
+++ exercicios/aula14/ex016/script.js
@@ -6,13 +6,16 @@ function contar() {
     let resp = document.getElementById("res")
         
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('ERRO! Faltam dados')
-        resp.innerHTML = "Impossível contar!"
+        resp.innerHTML = "Impossível contar! Faltam dados."
     } else {
         resp.innerHTML = "Contando . . . "
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p<=0) {
+            window.alert("Passo inválido. Considerando passo = 1")
+            p = 1
+        }
         if (i<f) {
             //Contagem crescente
             for (let c = i; c <= f; c += p) {
